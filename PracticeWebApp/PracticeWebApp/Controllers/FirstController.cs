279fd2e3b7638ed4f27bbc95b2eeb260aeae5239
Mayank <gptanaman8@gmail.com>
2026-05-29 23:09:51 +0530

    Add gitignore and remove generated files

---
+++ PracticeWebApp/PracticeWebApp/Controllers/FirstController.cs
@@ -0,0 +1,13 @@
+﻿//creating a controller in ASP.NET Core MVC
+using Microsoft.AspNetCore.Mvc;
+
+namespace PracticeWebApp.Controllers
+{
+    public class FirstController : Controller //inheritance
+    {
+        public IActionResult Index() //action method
+        {
+            return View(); //go to view folder and find the view with the same name as the action method
+        }
+    }
+}
---
+++ PracticeWebApp/PracticeWebApp/Program.cs
@@ -0,0 +1,36 @@
+namespace PracticeWebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=First}/{action=Index}/{id?}") //in controler folder, find the controller with the name "First" and then find the action method with the name "Index"
+                .WithStaticAssets();
+
+            app.Run();
+        }
+    }
+}
