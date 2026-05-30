0274beb6eac7567835e992935d1d701c0291631f
Mayank <gptanaman8@gmail.com>
2026-05-30 10:45:20 +0530

    Created child pages home,aboutUs etc and attached custom layout

---
+++ PracticeWebApp/PracticeWebApp/Controllers/GuestController.cs
@@ -0,0 +1,29 @@
+﻿//Five page Controller for Guest User
+using Microsoft.AspNetCore.Mvc;
+
+namespace PracticeWebApp.Controllers
+{
+    public class GuestController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
+        public IActionResult Gallery()
+        {
+            return View();
+        }
+    }
+}
